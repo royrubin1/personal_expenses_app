f30181f156eb3bc811491863d5302df7be023425
royrubin1 <royrubin@protonmail.com>
2023-02-07 18:56:23 +0100

    Class146

---
+++ lib/widgets/transaction_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import '../models/transaction.dart';
+import 'package:intl/intl.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key? key,
+    required this.transaction,
+    required this.deleteTx,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final Function deleteTx;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+      elevation: 5,
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: FittedBox(child: Text('\$${transaction.amount}')),
+          ),
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        subtitle: Text(DateFormat.yMMMMd().format(transaction.date)),
+        trailing: MediaQuery.of(context).size.width > 400
+            ? TextButton.icon(
+                icon: Icon(Icons.delete),
+                label: Text('Delete'),
+                style: TextButton.styleFrom(
+                  foregroundColor: Theme.of(context).errorColor,
+                ),
+                onPressed: () => deleteTx(transaction.id),
+              )
+            : IconButton(
+                icon: const Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () => deleteTx(transaction.id),
+              ),
+      ),
+    );
+  }
+}
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
+import 'transaction_item.dart';
+
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
   final Function deleteTx;
@@ -33,40 +35,8 @@ class TransactionList extends StatelessWidget {
           })
         : ListView.builder(
             itemBuilder: (context, index) {
-              return Card(
-                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
-                elevation: 5,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(6.0),
-                      child: FittedBox(
-                          child: Text('\$${transactions[index].amount}')),
-                    ),
-                  ),
-                  title: Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.titleLarge,
-                  ),
-                  subtitle: Text(
-                      DateFormat.yMMMMd().format(transactions[index].date)),
-                  trailing: MediaQuery.of(context).size.width > 400
-                      ? TextButton.icon(
-                          icon: Icon(Icons.delete),
-                          label: Text('Delete'),
-                          style: TextButton.styleFrom(
-                            foregroundColor: Theme.of(context).errorColor,
-                          ),
-                          onPressed: () => deleteTx(transactions[index].id),
-                        )
-                      : IconButton(
-                          icon: const Icon(Icons.delete),
-                          color: Theme.of(context).errorColor,
-                          onPressed: () => deleteTx(transactions[index].id),
-                        ),
-                ),
-              );
+              return TransactionItem(
+                  transaction: transactions[index], deleteTx: deleteTx);
             },
             itemCount: transactions.length,
           );
